743c40bcd24e0c775bf57f609b083dc0a3f16ca5
Md-Badhon-Sheikh <badhon0854@gmail.com>
2024-10-22 19:11:18 +0600

    added logical not js

---
+++ logical_not.js
@@ -0,0 +1,16 @@
+// let isLeader = true;
+// // if (isLeader===true)
+// if (isLeader) {
+//     console.log("Apnar jonno sob kisu free.")
+// }
+// else {
+//     console.log("Apnake tk dite hobe.")
+// }
+
+let isLeader = false;
+if (!isLeader) {
+    console.log("apnar jonno sob kisu free..")
+}
+else {
+    console.log("apnake sob kisur tk dite hobe..")
+}
